0c89fc29d365ae3e254229f31f764964a6b99f02
farraj007 <BARHAMFARRAJ@ICLOUD.COM>
2024-05-29 14:53:58 +0200

    Marking the path , with markers

--- launch/megoldas1.launch.py
+++ launch/megoldas1.launch.py
@@ -4,12 +4,6 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz01',
-            arguments=['-d', '$(find megoldas)/rviz/megoldas1.rviz']
-        ),
-        Node(
             package='pid_controller',
             executable='pid_error.py',
             name='pid_fal_kovetes',
