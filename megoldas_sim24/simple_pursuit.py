1a921f1275a50ab2b9b159b2ea7765f208d0f21e
Balázs Szőnyi <65489349+szonyibalazs@users.noreply.github.com>
2026-05-07 09:00:57 +0200

    get_param

--- megoldas_sim24/simple_pursuit.py
+++ megoldas_sim24/simple_pursuit.py
@@ -2,6 +2,7 @@ import math
 import numpy as np
 import rclpy
 from rclpy.node import Node
+from rcl_interfaces.msg import SetParametersResult
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist, Point, PointStamped, Transform
 from visualization_msgs.msg import MarkerArray, Marker
@@ -10,21 +11,23 @@ import tf2_ros
 import tf2_geometry_msgs
 import time
 
-
-KOZEPISKOLA_NEVE = "Ismeretlen kozepiskola"
-KOZEPISKOLA_AZON = "A00"
-ANGLE_RANGE = 360  # LSN10 LIDAR has 360 degrees scan
-VELOCITY = 1.00  # meters per second
-CAR_LENGTH = 0.445  # meters
-WHEELBASE = 0.3187  # meters
-MAP_FRAME = 'odom_combined'
-LASER_FRAME = 'laser'
-BASE_LINK_FRAME = 'base_link'
-
 class SimplePursuit(Node):
     def __init__(self):
-        global KOZEPISKOLA_NEVE, KOZEPISKOLA_AZON, ANGLE_RANGE, VELOCITY, CAR_LENGTH, WHEELBASE, MAP_FRAME, LASER_FRAME, BASE_LINK_FRAME
         super().__init__('simple_pursuit')
+        
+        self.declare_parameter('kozepiskola_neve', "Ismeretlen kozepiskola")
+        self.declare_parameter('kozepiskola_azon', "A00")
+        self.declare_parameter('angle_range', 360)
+        self.declare_parameter('velocity', 1.00)
+        self.declare_parameter('car_length', 0.445)
+        self.declare_parameter('wheelbase', 0.3187)
+        self.declare_parameter('map_frame', 'odom_combined')
+        self.declare_parameter('laser_frame', 'laser')
+        self.declare_parameter('base_link_frame', 'base_link')
+
+        self.get_params()
+        self.add_on_set_parameters_callback(self.parameters_callback)
+
         self.init_publishers()
         self.init_subscribers()
         self.init_markers()
@@ -35,27 +38,30 @@ class SimplePursuit(Node):
         self.prev_velocity = 0.0
         self.trans = Transform()
         self.timer = self.create_timer(1.0, self.timer_callback)
-        ## ROS2 parameters
-        self.declare_parameter('kozepiskola_neve', KOZEPISKOLA_NEVE)
-        self.declare_parameter('kozepiskola_azon', KOZEPISKOLA_AZON)
-        self.declare_parameter('angle_range', ANGLE_RANGE)
-        self.declare_parameter('velocity', VELOCITY)
-        self.declare_parameter('car_length', CAR_LENGTH)
-        self.declare_parameter('wheelbase', WHEELBASE)
-        self.declare_parameter('map_frame', MAP_FRAME)
-        self.declare_parameter('laser_frame', LASER_FRAME)
-        self.declare_parameter('base_link_frame', BASE_LINK_FRAME)
-        # ## ROS2 parameters
-        KOZEPISKOLA_NEVE = self.get_parameter('kozepiskola_neve').get_parameter_value().string_value
-        KOZEPISKOLA_AZON = self.get_parameter('kozepiskola_azon').get_parameter_value().string_value
-        ANGLE_RANGE = self.get_parameter('angle_range').get_parameter_value().integer_value
-        VELOCITY = self.get_parameter('velocity').get_parameter_value().double_value
-        CAR_LENGTH = self.get_parameter('car_length').get_parameter_value().double_value
-        WHEELBASE = self.get_parameter('wheelbase').get_parameter_value().double_value
-        MAP_FRAME = self.get_parameter('map_frame').get_parameter_value().string_value
-        LASER_FRAME = self.get_parameter('laser_frame').get_parameter_value().string_value
-        BASE_LINK_FRAME = self.get_parameter('base_link_frame').get_parameter_value().string_value
         self.get_logger().info("Simple Pursuit node has been started")
+
+    def get_params(self):
+        self.KOZEPISKOLA_NEVE = self.get_parameter('kozepiskola_neve').get_parameter_value().string_value
+        self.KOZEPISKOLA_AZON = self.get_parameter('kozepiskola_azon').get_parameter_value().string_value
+        self.ANGLE_RANGE = self.get_parameter('angle_range').get_parameter_value().integer_value
+        self.VELOCITY = self.get_parameter('velocity').get_parameter_value().double_value
+        self.CAR_LENGTH = self.get_parameter('car_length').get_parameter_value().double_value
+        self.WHEELBASE = self.get_parameter('wheelbase').get_parameter_value().double_value
+        self.MAP_FRAME = self.get_parameter('map_frame').get_parameter_value().string_value
+        self.LASER_FRAME = self.get_parameter('laser_frame').get_parameter_value().string_value
+        self.BASE_LINK_FRAME = self.get_parameter('base_link_frame').get_parameter_value().string_value
+
+    def parameters_callback(self, params):
+        for param in params:
+            if param.name == 'kozepiskola_neve':
+                self.KOZEPISKOLA_NEVE = param.value
+            elif param.name == 'kozepiskola_azon':
+                self.KOZEPISKOLA_AZON = param.value
+            elif param.name == 'velocity':
+                self.VELOCITY = param.value
+            elif param.name == 'wheelbase':
+                self.WHEELBASE = param.value
+        return SetParametersResult(successful=True)
 
     def init_publishers(self):
         self.pub = self.create_publisher(Twist, 'cmd_vel', 1)
@@ -74,7 +80,7 @@ class SimplePursuit(Node):
 
     def create_marker(self, r, g, b, ns):
         marker = Marker()
-        marker.header.frame_id = LASER_FRAME
+        marker.header.frame_id = self.LASER_FRAME
         marker.type = Marker.SPHERE_LIST
         marker.action = Marker.MODIFY
         marker.color.r = r
@@ -95,33 +101,29 @@ class SimplePursuit(Node):
     def calcPursuitAngle(self, goal_x, goal_y):
         alpha = math.atan2(goal_y, goal_x)
         lookahead_distance = math.sqrt(goal_x**2 + goal_y**2)
-        steering_angle = math.atan2(2.0 * WHEELBASE * np.sin(alpha) / lookahead_distance, 1)
+        steering_angle = math.atan2(2.0 * self.WHEELBASE * np.sin(alpha) / lookahead_distance, 1)
         return steering_angle
 
-    def calcPointPos(self, range, angle):
-        x = range * math.cos(angle)
-        y = range * math.sin(angle)
+    def calcPointPos(self, range_val, angle):
+        x = range_val * math.cos(angle)
+        y = range_val * math.sin(angle)
         return x, y
 
     def getDistance(self, ranges, angles):
         if len(ranges) <= 50:
             return 0.4
-
         max_x = -10.0
         lookup_right = self.get_lookup_indices(angles, -20, -0.1)
         lookup_left = self.get_lookup_indices(angles, 0.1, 20)
         lookup = np.concatenate((lookup_right, lookup_left))
-
         for t in lookup:
             point_x, _ = self.calcPointPos(ranges[t], angles[t])
             if not math.isinf(point_x) and point_x > max_x:
                 max_x = point_x
-
         if math.isinf(max_x):
             max_x = -5.0
         if max_x < 0.7:
             max_x = -0.5
-
         return max_x / 5.0
 
     def get_lookup_indices(self, angles, min_angle, max_angle):
@@ -132,9 +134,8 @@ class SimplePursuit(Node):
     def getAngle(self, ranges, angles):
         if len(ranges) <= 50:
             return 0.0, -99.0, 99.0
-
         left_d = self.get_side_distance(ranges, angles, 30, 60, self.marker_points_left)
-        right_d = self.get_side_distance(ranges, angles, 300, 330, self.marker_points_right) # 0-360°
+        right_d = self.get_side_distance(ranges, angles, 300, 330, self.marker_points_right)
         angle = (left_d + right_d) / 2
         if math.isinf(right_d):
             right_d = 99.0
@@ -142,14 +143,12 @@ class SimplePursuit(Node):
         if math.isinf(left_d):
             left_d = -99.0
             angle = 0.0
-
         return angle, left_d, right_d
 
     def get_side_distance(self, ranges, angles, min_angle, max_angle, marker, invert=False):
         min_index = np.where(math.radians(min_angle) < angles)[0][0]
         max_index = np.where(math.radians(max_angle) < angles)[0][0]
         indices = np.arange(min_index, max_index, 1)
-
         side_d = -10.0 if not invert else 10.0
         for t in indices:
             point_x, point_y = self.calcPointPos(ranges[t], angles[t])
@@ -159,39 +158,32 @@ class SimplePursuit(Node):
                 elif invert and point_y < side_d:
                     side_d = -point_y
             marker.points.append(Point(x=point_x, y=point_y))
-
         return side_d
 
     def followSimple(self, data):
         messageS1 = String()
         messageS1.data = "Simple_pursuit"
         angles = np.arange(data.angle_min, data.angle_max, data.angle_increment)
-        if abs(len(angles) - len(data.ranges)) > 1: # due to inaccuracies of float32, +/-1 difference is OK 
-            self.get_logger().warn("angles and ranges length differ " + str(len(angles)) + " vs " + str(len(data.ranges)))
-
         target_distance = self.getDistance(data.ranges, angles)
         target_angle, left_d, right_d = self.getAngle(data.ranges, angles)
-
         point = Point(x=target_distance, y=target_angle)
         point_st = PointStamped(point=point)
+        point_st.header.frame_id = self.LASER_FRAME
+        point_st.header.stamp = self.get_clock().now().to_msg()
         point_base_link_frame = None
-        
         try:
             point_base_link_frame = tf2_geometry_msgs.do_transform_point(point_st, self.trans)
             self.marker_points_goal.points.append(point_base_link_frame.point)
-            
         except:
-            self.get_logger().error("Error in transforming point")
-
+            pass
         self.publish_markers()
         if point_base_link_frame:
             self.update_message(messageS1, target_angle, left_d, right_d, target_distance, point_base_link_frame)
         self.pubst1.publish(messageS1)
-
         if point_base_link_frame:
             steering_err, velocity = self.calculate_control(point_base_link_frame)
         else:
-            steering_err, velocity = 0.0, 0.0  ####
+            steering_err, velocity = 0.0, 0.0
         return steering_err, velocity
 
     def publish_markers(self):
@@ -218,7 +210,6 @@ class SimplePursuit(Node):
             steering_err = self.calcPursuitAngle(point_base_link_frame.point.x, point_base_link_frame.point.y)
         except:
             steering_err = self.calcPursuitAngle(1, -1)
-
         steering_err = (steering_err + self.prev_steering_err) / 2
         velocity = (point_base_link_frame.point.x + self.prev_velocity) / 2
         self.prev_steering_err = steering_err
@@ -228,7 +219,7 @@ class SimplePursuit(Node):
     def callbackLaser(self, data):
         error_steering, velocity = self.followSimple(data)
         msg_cmd = Twist()
-        msg_cmd.linear.x = velocity * 0.5
+        msg_cmd.linear.x = velocity * 0.5 * self.VELOCITY
         msg_cmd.angular.z = error_steering
         if (self.is_running):
             self.pub.publish(msg_cmd)
@@ -236,12 +227,11 @@ class SimplePursuit(Node):
     def timer_callback(self):
         if self.first_run:
             try:
-                self.trans = self.buf.lookup_transform(BASE_LINK_FRAME, LASER_FRAME, rclpy.time.Time())
+                self.trans = self.buf.lookup_transform(self.BASE_LINK_FRAME, self.LASER_FRAME, rclpy.time.Time())
                 self.first_run = False
-            except tf2_ros.TransformException as ex:
-                self.get_logger().warn('Could not transform {"'+ BASE_LINK_FRAME + '"} {"' + LASER_FRAME + '"}') 
+            except tf2_ros.TransformException:
                 self.set_default_transform()
-        self.pubst2.publish(String(data=f"{KOZEPISKOLA_NEVE} ({KOZEPISKOLA_AZON})"))
+        self.pubst2.publish(String(data=f"{self.KOZEPISKOLA_NEVE} ({self.KOZEPISKOLA_AZON})"))
 
     def set_default_transform(self):
         self.trans.translation.x = 0.26
@@ -253,40 +243,28 @@ class SimplePursuit(Node):
         self.trans.rotation.w = 0.000796326710733
 
     def shutdown_node(self):
-        self.get_logger().info('Simple Pursuit shutdown procedure has been started')
         self.is_running = False
-        # Publish a stop (0 speed) command before shutdown
         try:   
             twist_cmd = Twist()
             twist_cmd.linear.x = 0.0
             twist_cmd.angular.z = 0.0
             self.pub.publish(twist_cmd)
-            # Wait 0.5 sec to make sure
             time.sleep(0.5)
             self.pub.publish(twist_cmd)
-            self.get_logger().info('Simple Pursuit node has been stopped')
-        except Exception as e:
-            print(f"An error occurred: {e}") # print is OK here instead of self.get_logger().info
+        except:
+            pass
 
 def main(args=None):
-    rclpy.init(signal_handler_options=rclpy.SignalHandlerOptions(2))
+    rclpy.init(args=args, signal_handler_options=rclpy.SignalHandlerOptions(2))
     node = SimplePursuit()
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
         pass
     finally:
-        try:
-            node.shutdown_node()
-            node.destroy_node()
-            rclpy.try_shutdown()
-        except Exception as e:
-            print(f"An error occurred: {e}") # print is OK here instead of self.get_logger().info
+        node.shutdown_node()
         node.destroy_node()
         rclpy.try_shutdown()
-
-
-
 
 if __name__ == '__main__':
     main()
